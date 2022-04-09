52efc835b2a422ec76798fa417201131be81f1d9
Pizza Mozzarella <163glonger@gmail.com>
2022-04-09 19:29:59 +0400

    fix

---
+++ Anketa.cs
@@ -0,0 +1,14 @@
+﻿public static class Anketa
+{
+	public static void LaunchApp()
+    {
+        Console.WriteLine("Укажите ваши данные\n");
+        Name.GetName();
+        Age.GetAge();
+        Sex.GetSex();
+        Hobbies.GetHobbies();
+        About.GetAbout();
+        Preferences.GetPreferences();
+        Console.WriteLine($"Ваше Имя: {Name.name}\nВаш Пол: {Sex.sex}\nВаши Хобби: {Hobbies.hobbies}\nО вас: {About.about}\nПидорас? {Preferences.preferences}");
+    }
+}
